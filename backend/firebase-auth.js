ba3b256e1447f167ffd0c69de8c22e8ef4284096
Samuel Monteiro Dos Santos <carolina31serragrande@gmail.com>
2026-05-06 08:18:00 -0300

    Update Firebase import and fix event listener syntax

--- backend/firebase-auth.js
+++ backend/firebase-auth.js
@@ -1,4 +1,4 @@
-import { sendEmailVerification } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js';
 import {
   getAuth,
   GoogleAuthProvider,
